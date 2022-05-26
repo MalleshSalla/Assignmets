964bc29b94c070fe1c52ee9bcd37b02fbf63dc03
smallesh <smallesh@LIN34003924.corp.capgemini.com>
2022-05-26 19:46:21 +0530

    added assignment5

--- corejava_assignments/src/com/corejava/assignment5/Application.java
+++ corejava_assignments/src/com/corejava/assignment5/Application.java
@@ -5,45 +5,27 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Random;
 
+import java.util.HashMap;
+
 public class Application {
 
 	public static void main(String[] args) {
+
+		HashMap<Integer,Double> h = new HashMap<>();
 		
-
-		Map<Integer, Double> hashmap = new HashMap<Integer, Double>();
-		Random random = new Random();
-	
-		for (int i = 1; i <= 10; i++) {
-
-			hashmap.put((Integer)random.nextInt(50),(Double)random.nextDouble(50.00));
-		}
-
-		for (Entry<Integer, Double> entry : hashmap.entrySet()) {
-			System.out.println(entry.getKey() + " : " + entry.getValue());
-		}
-
-	}
-
+		// put() is used to add key and value inside Map
+		h.put(10, 22.43);
+		h.put(20, 20.55);
+		h.put(30, 25.23);
+		h.put(40, 26.35);
+		h.put(50, 26.43);
+		h.put(60, 27.55);
+		h.put(70, 28.88);
+		h.put(80, 29.95);
+		h.put(90, 29.75);
+		h.put(100,30.22);
+		System.out.println(h);
+}
 }
 
-/*
- * HashMap<Integer, Double> companies = new HashMap<>();
- * companies.putAll(12,90.0); companies.put("Paypal", "North San Jose");
- * companies.put("Google", "Mountain View"); companies.put("Yahoo",
- * "Santa Clara"); companies.put("Twitter", "San Francisco");
- * 
- * // Get a random entry from the HashMap. Object[] crunchifyKeys =
- * companies.keySet().toArray(); Object key = crunchifyKeys[new
- * Random().nextInt(crunchifyKeys.length)];
- * System.out.println("**** Random Value **** \n" + key + " :: " +
- * companies.get(key));
- * 
- * List<Map.Entry<String, String>> list = new ArrayList<Map.Entry<String,
- * String>>(companies.entrySet());
- * 
- * // How to Shuffle a List?? // Each time you get a
- * different order...
- * System.out.println("\n***** Now Let's start shuffling list *****");
- * Collections.shuffle(list); for (Map.Entry<String, String> entry : list) {
- * System.out.println(entry.getKey() + " :: " + entry.getValue());
- */
+
